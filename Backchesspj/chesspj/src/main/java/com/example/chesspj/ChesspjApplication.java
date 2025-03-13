c08c28c4078190a284617cb5f2f6eff661619375
Sw3342_99 <106459398+Kimdudcjf@users.noreply.github.com>
2025-03-13 19:48:32 +0900

    초기 설정
    초기 설정

---
+++ Backchesspj/chesspj/src/main/java/com/example/chesspj/ChesspjApplication.java
@@ -0,0 +1,13 @@
+package com.example.chesspj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesspjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChesspjApplication.class, args);
+	}
+
+}
---
+++ Backchesspj/chesspj/src/test/java/com/example/chesspj/ChesspjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chesspj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChesspjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
